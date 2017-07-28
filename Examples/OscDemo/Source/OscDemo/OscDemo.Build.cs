6bad1ef023eb47631a4c6924b4a4743a80b4b77d
tkuehnl <todd.kuehnl@pixogroup.com>
2017-07-28 01:50:41 -0400

    additional fixes for 4.17

--- Examples/OscDemo/Source/OscDemo/OscDemo.Build.cs
+++ Examples/OscDemo/Source/OscDemo/OscDemo.Build.cs
@@ -4,23 +4,11 @@ using UnrealBuildTool;
 
 public class OscDemo : ModuleRules
 {
-	public OscDemo(TargetInfo Target)
+	public OscDemo(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
 	}
 }
